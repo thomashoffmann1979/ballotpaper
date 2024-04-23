1664eed70f95ea85d3cd5bc18744d27893f18d19
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2024-04-23 10:59:14 +0200

    test sequential processing of images

--- processImage.go
+++ processImage.go
@@ -2,11 +2,35 @@ package main
 
 import (
 	"log"
-	//"gocv.io/x/gocv"
+	"gocv.io/x/gocv"
+	"github.com/bieber/barcode"
 )
 
+
+
+func findBarcodes(scanner *barcode.ImageScanner, img gocv.Mat)[]BarcodeSymbol{
+	//smaller:=ResizeMat(img, img.Cols() / barcodeScale, img.Rows() /barcodeScale)
+	smaller:=gocv.NewMat()
+	gocv.CvtColor(img, &smaller, gocv.ColorBGRToGray)
+	log.Println("findBarcodes",barcodeScale)
+	symbols, err := scanner.ScanMat(&smaller)
+	if err != nil {
+		panic(err)
+	}
+	log.Println("findBarcodes",len(symbols))
+	syms := []BarcodeSymbol{}
+	for _, s := range symbols {
+		syms = append(syms,BarcodeSymbol{Type:s.Type.Name(),Data:s.Data,Quality:s.Quality,Boundary:s.Boundary})
+		log.Println("BarcodeSymbol",s.Type.Name(),s.Data,s.Quality,s.Boundary)
+	}
+	return syms
+}
+
 func processImage(){
-	
+	scanner := barcode.NewScanner()
+	scanner.SetEnabledAll(false)
+	scanner.SetEnabledSymbology(barcode.Code39,true)
+	scanner.SetEnabledSymbology(barcode.Code128,true)
 	log.Println("processImage starting ")
 	for {
 		if !runVideo {
@@ -17,6 +41,26 @@ func processImage(){
 		img,ok := <-paperChannelImage
 		if ok {
 			log.Println("got image",ok,img.Size())
+
+			if !img.Empty() {
+				contour := findPaperContour(img)
+				cornerPoints := getCornerPoints(contour)
+				topLeftCorner := cornerPoints["topLeftCorner"]
+				bottomRightCorner := cornerPoints["bottomRightCorner"]
+				if false {
+					log.Printf("template: %d %d",  bottomRightCorner.X-topLeftCorner.X, bottomRightCorner.Y-topLeftCorner.Y )
+				}
+
+				paper := extractPaper(img, contour, bottomRightCorner.X-topLeftCorner.X, bottomRightCorner.Y-topLeftCorner.Y, cornerPoints)
+				mean := paper.Mean()
+				if (mean.Val1+mean.Val2+mean.Val3)/3 > 150 {
+					area := float64(paper.Size()[0]) * float64(paper.Size()[1]) / float64(img.Size()[0]) / float64(img.Size()[1])
+					log.Println("mean",mean.Val1,mean.Val2,mean.Val3,area,paper.Size())
+					if area > 0.1 {
+						findBarcodes(scanner,paper)
+					}
+				}
+			}
 		}
 		log.Println("processImage done",runVideo)
 	}
